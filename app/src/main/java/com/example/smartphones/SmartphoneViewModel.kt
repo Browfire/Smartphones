817713697716f31e7d588f8b6f12cb9541d8ff65
LXZ <l.aguirre01@ufromail.cl>
2020-12-05 12:21:16 -0300

    Changes in ViewModel -> added smartphone selection

--- app/src/main/java/com/example/smartphones/SmartphoneViewModel.kt
+++ app/src/main/java/com/example/smartphones/SmartphoneViewModel.kt
@@ -3,6 +3,7 @@ package com.example.smartphones
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import com.example.smartphones.model.SmartphoneDetails
 import com.example.smartphones.model.SmartphoneItem
 import com.example.smartphones.model.local.SmartphoneDataBase
@@ -12,6 +13,7 @@ class SmartphoneViewModel(application: Application): AndroidViewModel(applicatio
     private val myRepository: SmartphoneRepository
 
     val allSmartphones: LiveData<List<SmartphoneItem>>
+    val smartphoneSelection = MutableLiveData<Int>()
 
     init {
         val myDao = SmartphoneDataBase.getDatabase(application).smartphoneDao()
@@ -23,6 +25,10 @@ class SmartphoneViewModel(application: Application): AndroidViewModel(applicatio
     fun getOneSmartphoneDetails(id: Int): LiveData<SmartphoneDetails>{
         myRepository.getSmartphoneDetailsFromApi(id)
         return myRepository.getSmartphoneDetails(id)
+    }
+
+    fun smartphoneSelected (smartphoneId: Int){
+        smartphoneSelection.value = smartphoneId
     }
 
 }
